de1a09c6fe3918a4c061eb56791ef56cc6b5ae2f
kevin <kevinn.vieira22@gmail.com>
2026-09-09 15:04:47 -0300

    Adição d novos campos de imagem e texto, e estilização.

--- App.tsx
+++ App.tsx
@@ -1,26 +1,48 @@
 import { Ionicons } from "@expo/vector-icons";
-import { ScrollView, StyleSheet, Text, View } from 'react-native';
+import { Image, ScrollView, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <ScrollView style={style.container}>
-        <View style={style.header}>
-          <View>
-            <Text>Café do Código</Text>
-            <Text>Seu café, uma linha por vez </Text>
-          </View>
-
-            <Ionicons name="person" size = {20} color = "#2f2d2c"></Ionicons>
+    <ScrollView style={styles.container}>
+      <View style={styles.header}>
+        <View>
+          <Text style={styles.headerTitle}>Café do Código</Text>
+          <Text style={styles.headerSubtitle}>Seu café, uma linha por vez </Text>
         </View>
+
+        <View style={styles.avatarPlaceholder}>
+          <Ionicons name="person" size={20} color="#2f2d2c"></Ionicons>
+        </View>
+
+      </View>
+      {/*Header*/}
+
+      {/*Conteudo*/}
+      <View style={styles.content}>
+
+        <View style={styles.saudacao}>
+          <Text style={styles.conteudoTitle}>Bom dia!</Text>
+          <Text style={styles.conteudoSubtitle}>Que tal um café hoje?</Text>
+        </View>
+
+        <View>
+          <Image source={require("./assets/coffee.jpg")}></Image>
+          <Text>Cappucino Especial</Text>
+          <Text>Cremoso e delicioso</Text>
+          <Text>R$ 12,90</Text>
+        </View>
+
+      </View>
+      {/*Conteudo*/}
     </ScrollView>
 
   );
 }
 
-const style = StyleSheet.create({
+const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#f9f9f9',
+    backgroundColor: '#A98467',
 
   },
   header: {
@@ -31,6 +53,48 @@ const style = StyleSheet.create({
     flexDirection: "row",
     justifyContent: "space-between",
     alignItems: "center"
+  },
+
+  headerTitle: {
+    fontSize: 22,
+    fontWeight: "800",
+    color: "#2f2d2c"
+  },
+
+  headerSubtitle: {
+    fontSize: 14,
+    color: "#441a1a",
+    marginTop: 4
+  },
+
+  avatarPlaceholder: {
+    width: 44,
+    height: 44,
+    borderRadius: 22,
+    backgroundColor: "#f0f0f0",
+    justifyContent: "center",
+    alignItems: "center"
+  },
+
+  saudacao: {
+    marginBottom: 24,
+    marginTop: 10,
+  },
+
+  content: {
+    paddingHorizontal: 24
+  },
+
+  conteudoTitle: {
+    fontSize: 32,
+    fontWeight: "800",
+    color: "#2f2d2c"
+  },
+
+  conteudoSubtitle: {
+    fontSize: 16,
+    color: "#441a1a",
+    marginTop: 8
   }
 })
 
